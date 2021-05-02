e43ac953599334174a8c32af6cf6250be407c926
John Wang <johncwang@gmail.com>
2021-05-02 04:33:13 -0700

    feat: cli: init

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/grokify/simplego/encoding/jsonutil"
+	"github.com/grokify/simplego/net/http/httpsimple"
+	"github.com/jessevdk/go-flags"
+
+	"github.com/go-xrp/go-rippled/data"
+)
+
+type Options struct {
+	Method   string `short:"m" long:"method" description:"Method" required:"true"`
+	Data     string `short:"d" long:"data" description:"Data"`
+	TestData []bool `short:"t" long:"testdata" description:"Use Test Data"`
+	Exec     []bool `short:"x" long:"exec" description:"Execute API Call"`
+	Verbose  []bool `short:"v" long:"verbose" description:"Verbose"`
+	Pretty   []bool `short:"p" long:"pretty" description:"Pretty Print Result"`
+}
+
+const (
+	RippledJsonRpcUrl = "https://s1.ripple.com:51234/"
+)
+
+func main() {
+	var opts Options
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reqBodyBytes := []byte{}
+
+	if len(opts.Data) > 0 {
+		reqBodyBytes = []byte(opts.Data)
+	} else if len(opts.TestData) > 0 {
+		reqBodyBytes, err = data.ExampleJsonRequest(opts.Method)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if len(opts.Verbose) > 0 {
+		fmt.Println(string(reqBodyBytes))
+	}
+
+	if len(opts.Exec) > 0 {
+		req := httpsimple.SimpleRequest{
+			Method: http.MethodPost,
+			URL:    RippledJsonRpcUrl,
+			Body:   reqBodyBytes,
+			IsJSON: true}
+
+		resp, err := httpsimple.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resBodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(opts.Pretty) > 0 {
+			fmt.Println(string(jsonutil.PrettyPrint(resBodyBytes, "", "  ")))
+		} else {
+			fmt.Println(string(resBodyBytes))
+		}
+	}
+	fmt.Println("DONE")
+}
